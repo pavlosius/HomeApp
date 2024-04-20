b36637e34b041527826bcddc6765d9631b081fef
pavlosius <pavlosius@mail.ru>
2024-04-20 21:27:53 +0300

    Add project files.

---
+++ HomeApp/HomeApp.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeApp.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new HomeApp.App());
+        }
+    }
+}
---
+++ HomeApp/HomeApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using HomeApp.Pages;
+using Xamarin.Forms;
+
+namespace HomeApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new LoadingPage();
+            //MainPage = new LoginPage();
+            MainPage = new RoomsPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
